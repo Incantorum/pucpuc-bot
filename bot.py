2bff68ac107972d1159e8b96d0de317b8de8278c
AlanTellezS <43708959+AlanTellezS@users.noreply.github.com>
2019-04-22 21:59:19 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,13 @@ info = [
     ["'1-3 Star Ema List'!AT3:AW", "Zaregoto"]
 ]
 
+commands = [
+    ["help", "Info about a command.\n\tExample: $help ema"],
+    ["searchEma", "Search through the 1-3 ema list by star and skill, you can also use 'any' as a parameter.\n\tExample: $searchEma 1;J"],
+    ["searchCharEma", "Search through the 4-5 ema list by name \nExample: $searchCharEma Araragi"],
+    ["ema", "Get the description of a 4-5 ema by using its number on the doc (You can use searchCharEma to know that number)\nExample: $ema 10"]
+]
+
 server_default_thumbnail = "https://cdn.discordapp.com/attachments/492461461113667605/568033372543385611/cha_block_madoka01_v01-CAB-c50120ac711700ee630d6512935a44fe-1479165618584560336.png"
 
 permited_ema_stars = ['1','2','3','4','5','ANY','Any','any']
@@ -33,34 +40,57 @@ async def on_message(message):
     channel = message.channel
 
     if message.content.startswith('$commands'):
-        msg = '$searchEma\n'
-        msg = msg + '$searchCharEma\n'
-        msg = msg + '$ema\n'
+        msg = ''
+        for command in commands:
+            msg = '$' + command[0] + "\n"
         embed_msg = generic_embed("Commands", msg, "", server_default_thumbnail)
         await channel.send(embed = embed_msg)
-
-    elif message.content.startswith('$searchEma'):
-        emaList = loadEmaList1_3()
-        msg = ""
-        find = message.content.split(";")
-        find[2] = find[2].upper()
-        find[1] = find[1].upper()
-        if ((len(find) != 3) or not(find[1] in permited_ema_stars) or not(find[2] in permited_ema_skill)):
+    
+    # $help
+    if message.content.startswith(commands[0][0]):
+        command_found = False
+        cmd = message.content.split(" ")
+        if (len(cmd) != 2):
             await channel.send(embed = error_embed())
         else:
-            for arc in emaList:
-                for ema in emaList[arc]:
-                    print("{find[2]} - {find[2]}")
-                    if((find[2]==ema[1] or find[2]=="ANY") and (find[1]==ema[2] or find[1].upper=="ANY")):
-                        print("added")
-                        msg = msg + "\t%s - %s - %s\n" % (ema[0],ema[1],ema[2])
-            if(len(msg) > 2048):
-                embed_msg = error_embed(error="Result too big")
+            cmd = cmd[1]
+            for i in range (0, len(commands)):
+                if (cmd == command[i][0]):
+                    msg = command[i][1]
+                    command_found = True
+            if command_found == True:
+                await channel.send(embed = generic_embed("Command " + cmd, msg, "", server_default_thumbnail))
             else:
-                embed_msg = generic_embed("Ema Found", msg, "", server_default_thumbnail)
-            await channel.send(embed = embed_msg)
+                await channel.send(embed = error_embed(error = "Command not found"))
+
+    # $searchEma
+    elif message.content.startswith(commands[1][0]):
+        emaList = loadEmaList1_3()
+        msg = ""
+        find = message.content.split(" ")
+        find = find.split(";")
+        if (len(find) != 2):
+            await channel.send(embed = error_embed())
+        else:
+            if (not(find[1] in permited_ema_stars) or not(find[2] in permited_ema_skill)):
+                await channel.send(embed = error_embed(error = "Wrong Parameters"))
+            else:
+                find[2] = find[2].upper()
+                find[1] = find[1].upper()
+                for arc in emaList:
+                    for ema in emaList[arc]:
+                        print("{find[2]} - {find[2]}")
+                        if((find[2]==ema[1] or find[2]=="ANY") and (find[1]==ema[2] or find[1].upper=="ANY")):
+                            print("added")
+                            msg = msg + "\t%s - %s - %s\n" % (ema[0],ema[1],ema[2])
+                if(len(msg) > 2048):
+                    embed_msg = error_embed(error="Result too big")
+                else:
+                    embed_msg = generic_embed("Ema Found", msg, "", server_default_thumbnail)
+                await channel.send(embed = embed_msg)
     
-    elif message.content.startswith('$searchCharEma'):
+    # $searchCharEma
+    elif message.content.startswith(commands[2][0]):
         emaList4_5 = loadEmaList4_5()
         msg = ""
         find = message.content.split()
@@ -73,7 +103,8 @@ async def on_message(message):
         embed_msg = generic_embed("Ema found", msg, "", server_default_thumbnail)
         await channel.send(embed = embed_msg)
     
-    elif message.content.startswith('$ema'):
+    # $ema
+    elif message.content.startswith(commands[3][0]):
         emaList4_5 = loadEmaList4_5()
         msg = ""
         find = message.content.split()
